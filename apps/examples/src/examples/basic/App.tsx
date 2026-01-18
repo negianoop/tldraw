2479a7aaf1d93991b93823ad3fc8cbd0c2c8cb30
Anoop <84010126+negianoop@users.noreply.github.com>
2026-01-18 05:28:03 +0000

    experiment-keybindings

--- apps/examples/src/examples/basic/App.tsx
+++ apps/examples/src/examples/basic/App.tsx
@@ -1,14 +1,31 @@
-import { Tldraw } from 'tldraw'
+import { Tldraw, TLUiOverrides } from 'tldraw'
 import 'tldraw/tldraw.css'
+
+const myOverrides: TLUiOverrides = {
+	actions(editor, actions) {
+		// You can delete actions, but remember to
+		// also delete the menu items that reference them!
+		delete actions['insert-embed']
+
+		// Create a new action or replace an existing one
+		actions['my-new-action'] = {
+			id: 'my-new-action',
+			label: 'My new action',
+			readonlyOk: true,
+			kbd: 'cmd+u,alt+u',
+			onSelect(source: any) {
+				// Whatever you want to happen when the action is run
+				window.alert('My new action just happened!')
+			},
+		}
+		return actions
+	},
+}
 
 export default function BasicExample() {
 	return (
 		<div className="editor">
-			<Tldraw
-				onMount={(editor) => {
-					editor.selectAll()
-				}}
-			/>
+			return <Tldraw overrides={[myOverrides]} />
 		</div>
 	)
 }
